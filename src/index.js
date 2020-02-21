f8a1ddd7461d9ade8970fabda03297d19b03b554
arebeb <romakislitsin89@gmail.com>
2020-02-22 03:19:57 +0600

    feat: task solved

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 
 exports.min = function min (array) {
+  if (Array.isArray(array) && (array.length > 0))
+    return array.sort(function(a, b) { return a - b }).reverse().pop()
   return 0;
 }
 
 exports.max = function max (array) {
+  if (Array.isArray(array) && (array.length > 0))
+    return array.sort(function(a, b) { return a - b }).pop()
   return 0;
 }
 
 exports.avg = function avg (array) {
+  if (Array.isArray(array) && (array.length > 0))
+    return array.reduce(function(a, b) { return a + b }) / array.length
   return 0;
 }
